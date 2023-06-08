874f3d1a54bb0cb221c7ac03e6ea1fc2a8cc4581
JackBrens <574950259@qq.com>
2023-06-09 02:58:29 +0800

    feat: 🚀 新增eslint、pretter、husky、commitlint等插件保证代码质量

--- loaders/clear-log-loader/index.js
+++ loaders/clear-log-loader/index.js
@@ -1,3 +1,3 @@
-module.exports = function (content, map, meta) {
-  return content.replace(/console\.log\(.*\);?/g, '')
+module.exports = function (content) {
+	return content.replace(/console\.log\(.*\);?/g, '')
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
-import VueRouter from "vue-router";
-import router from "./router";
+import VueRouter from 'vue-router'
+import router from './router'
 import Vue from 'vue'
 import App from './App'
 Vue.use(VueRouter)
@@ -9,6 +9,6 @@ Vue.config.productionTip = false
 console.log('hello main')
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: (h) => h(App),
 }).$mount('#app')
